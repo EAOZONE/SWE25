c3add07e7208b741e7cc5191d3d8a178b6609035
jadevega <116754119+jadevega@users.noreply.github.com>
2025-03-16 16:01:18 -0400

    added homepage

---
+++ frontend/src/Homepage.js
@@ -0,0 +1,24 @@
+
+import MyImage from "./—Pngtree—glass jar and love png_4425351 (1).png"
+
+import "./Homepage.css";
+
+import {useNavigate} from "react-router-dom";
+
+const Home = () => {
+    const navigate = useNavigate();
+
+    return(
+    <div className= "home-container">
+        <h1 className ="text"> Welcome to Jar of Joy!!!  </h1>
+        <h5 className ="summary"> 2025 SWE Project </h5>
+        <img src={MyImage} alt="Jar of Joy" />
+        <div className= "button-container">
+        <button onClick={() => navigate("/login")}>Login</button>
+        <button onClick={() => navigate("/register")}>Register</button>
+        </div>
+    </div>
+    )
+
+}
+export default Home;
