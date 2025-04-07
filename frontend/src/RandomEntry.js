fc6fedbe4cc8da95c26b7b58075f231657348d78
Ben <bpratt@ihmc.org>
2025-04-07 13:42:19 -0400

    Post your entry to twitter(x)

--- frontend/src/RandomEntry.js
+++ frontend/src/RandomEntry.js
@@ -29,6 +29,12 @@ function RandomEntry() {
     }
   };
 
+  const handleTweet = () => {
+    const tweetText = encodeURIComponent(`${entry.content}\n#JarOfJoy #Gratitude`);
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${tweetText}`;
+    window.open(tweetUrl, "_blank");
+};
+
   useEffect(() => {
     if (!fetchCalled.current) {
       fetchCalled.current = true;
@@ -37,25 +43,28 @@ function RandomEntry() {
   }, []);
 
   return (
-    <div className="random-entry-container">
-      <h2>Random Gratitude Entry</h2>
-      {loading && <p>Loading...</p>}
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <div className="random-entry-container">
+        <h2>Random Gratitude Entry</h2>
+        {loading && <p>Loading...</p>}
+        {error && <p style={{color: 'red'}}>{error}</p>}
 
-      {!loading ? (
-        entry ? (
-        <div className="random-entry">
-          <p>{entry.content}</p>
-          <small>{new Date(entry.time).toLocaleString()}</small>
-        </div>
-      ) : (
-        <p>No entries found. Start writing gratitude entries!</p>
-      )):null}
-      <button onClick={fetchRandomEntry}>Get Random Entry</button>
-      <button type="home-btn" onClick={() => navigate('/home')}>
-        Home
-      </button>
-    </div>
+        {!loading ? (
+            entry ? (
+                <div className="random-entry">
+                  <p>{entry.content}</p>
+                  <small>{new Date(entry.time).toLocaleString()}</small>
+                </div>
+            ) : (
+                <p>No entries found. Start writing gratitude entries!</p>
+            )) : null}
+        <button onClick={fetchRandomEntry}>Get Random Entry</button>
+        <button onClick={handleTweet}>
+          Share on Twitter
+        </button>
+        <button type="home-btn" onClick={() => navigate('/home')}>
+          Home
+        </button>
+      </div>
   );
 }
 
